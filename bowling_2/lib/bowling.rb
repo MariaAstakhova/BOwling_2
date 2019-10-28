c68cccf8b5835b342972a8feeec50f51562a5581
MariaAstakhova <54268940+MariaAstakhova@users.noreply.github.com>
2019-10-28 09:02:16 +0000

    Update bowling.rb

--- bowling_2/lib/bowling.rb
+++ bowling_2/lib/bowling.rb
@@ -1,35 +1,34 @@
 class Bowling
 
 
-    def strike?(roll)
-        roll == 10
-    end
+def strike?(roll)
+ roll == 10
+end
 
-    def spare?(frame)
-        frame.sum == 10
-    end
+def spare?(frame)
+ frame.sum == 10
+end
 
-    def score(line)
-        total = 0
+def score(line)
+ total = 0
        
-        line.each_with_index do |frame, roll| 
-            total+=frame.sum
+ line.each_with_index do |frame, roll| 
+  total+=frame.sum
             
-            if strike?(frame[0]) && roll != 9
-                    next_roll = roll+1
-                    total += line[next_roll].sum
+  if strike?(frame[0]) && roll != 9
+   next_roll = roll+1
+   total += line[next_roll].sum
                     
-                    if strike?(line[next_roll][0])
-                        second_next_roll = roll+2
-                        total += line[second_next_roll][0]
-                    end  
-            elsif spare?(frame) 
-                total+=line[roll+1][0] unless roll == 9
-            end
-        end
+   if strike?(line[next_roll][0])
+    second_next_roll = roll+2
+    total += line[second_next_roll][0]
+   end  
+  elsif spare?(frame) 
+   total+=line[roll+1][0] unless roll == 9
+   end
+  end
        
-        total
+total
         
-    end
-
+end
 end
